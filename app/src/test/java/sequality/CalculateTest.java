3490877df4aa8f2320ee926f1f660788d7b6325a
kosakatm <m1m21a19@oit.ac.jp>
2021-07-12 13:50:06 +0900

    kadai2_2

--- app/src/test/java/sequality/CalculateTest.java
+++ app/src/test/java/sequality/CalculateTest.java
@@ -5,28 +5,32 @@ import static org.junit.Assert.*;
 
 public class CalculateTest
 {
-  @Test public void testSum()
+  @Test
+  public void testSum()
   {
     Calculate calculate = new Calculate();
     int expected = 5;
     assertEquals(expected, calculate.sum(2, 3));
   }
 
-  @Test public void testSumAve()
+  @Test
+  public void testSumAve()
   {
     Calculate calculate = new Calculate();
     int expected = 13.5;
     assertEquals(expected, calculate.sumAve(9, 18));
   }
 
-  @Test public void testTotal()
+  @Test
+  public void testTotal()
   {
     Calculate calculate = new Calculate();
     int expected = 135;
     assertEquals(expected, calculate.total(9, 18));
   }
 
-  @Test public void testTotalAve()
+  @Test
+  public void testTotalAve()
   {
     Calculate calculate = new Calculate();
     int expected = 58.5;
